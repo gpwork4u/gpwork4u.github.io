7aae86e8017fe5b7b600bd0e269f7f860794dd2c
gpwork4u <gpwork4u@gmail.com>
2021-12-24 09:35:16 +0800

    Adjust post link url

--- components/post-preview.js
+++ components/post-preview.js
@@ -13,7 +13,7 @@ export default function PostPreview({
       <div className="mb-5">
       </div>
       <h3 className="text-3xl mb-3 leading-snug">
-        <Link as={`/posts/${slug}`} href="/posts/[slug]">
+        <Link as={`./posts/${slug}`} href="./posts/[slug]">
           <a className="hover:underline">{title}</a>
         </Link>
       </h3>
