4b0cecd8c2ad72d04c1eb123ee5faa7047275c69
gpwork4u <gpwork4u@gmail.com>
2022-02-07 13:10:38 +0800

    Add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,22 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_ID}`}></script>
+          <script
+            dangerouslySetInnerHTML={{
+                __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', ${process.env.GA_ID}, {
+                page_path: window.location.pathname,
+              });
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
